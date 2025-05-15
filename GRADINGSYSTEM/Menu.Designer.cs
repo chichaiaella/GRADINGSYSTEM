c1545aaa8cccb164f508c5b2589f54e8d603cf2f
chichai <nigietenebroso@gmail.com>
2025-05-16 01:06:52 +0800

    name changes

--- GRADINGSYSTEM/Menu.Designer.cs
+++ GRADINGSYSTEM/Menu.Designer.cs
@@ -29,27 +29,27 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlHeader = new System.Windows.Forms.Panel();
             this.btnLogout = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.pnlSubHeader = new System.Windows.Forms.Panel();
+            this.pnlMenuBar = new System.Windows.Forms.Panel();
             this.btnAbout = new System.Windows.Forms.Button();
             this.btnGrades = new System.Windows.Forms.Button();
             this.pnlDisplay = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.pnlHeader.SuspendLayout();
+            this.pnlMenuBar.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // pnlHeader
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.panel1.Controls.Add(this.btnLogout);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.ForeColor = System.Drawing.Color.Black;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(584, 33);
-            this.panel1.TabIndex = 0;
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.pnlHeader.Controls.Add(this.btnLogout);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.ForeColor = System.Drawing.Color.Black;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(584, 33);
+            this.pnlHeader.TabIndex = 0;
             // 
             // btnLogout
             // 
@@ -61,25 +61,25 @@
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
-            // panel2
+            // pnlSubHeader
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 33);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(584, 10);
-            this.panel2.TabIndex = 1;
+            this.pnlSubHeader.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlSubHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlSubHeader.Location = new System.Drawing.Point(0, 33);
+            this.pnlSubHeader.Name = "pnlSubHeader";
+            this.pnlSubHeader.Size = new System.Drawing.Size(584, 10);
+            this.pnlSubHeader.TabIndex = 1;
             // 
-            // panel3
+            // pnlMenuBar
             // 
-            this.panel3.BackColor = System.Drawing.Color.Transparent;
-            this.panel3.Controls.Add(this.btnAbout);
-            this.panel3.Controls.Add(this.btnGrades);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 43);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(584, 58);
-            this.panel3.TabIndex = 2;
+            this.pnlMenuBar.BackColor = System.Drawing.Color.Transparent;
+            this.pnlMenuBar.Controls.Add(this.btnAbout);
+            this.pnlMenuBar.Controls.Add(this.btnGrades);
+            this.pnlMenuBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMenuBar.Location = new System.Drawing.Point(0, 43);
+            this.pnlMenuBar.Name = "pnlMenuBar";
+            this.pnlMenuBar.Size = new System.Drawing.Size(584, 58);
+            this.pnlMenuBar.TabIndex = 2;
             // 
             // btnAbout
             // 
@@ -128,25 +128,25 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(584, 749);
             this.Controls.Add(this.pnlDisplay);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlMenuBar);
+            this.Controls.Add(this.pnlSubHeader);
+            this.Controls.Add(this.pnlHeader);
             this.Name = "Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Menu_FormClosing);
             this.Load += new System.EventHandler(this.Menu_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlMenuBar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Panel pnlSubHeader;
+        private System.Windows.Forms.Panel pnlMenuBar;
         private System.Windows.Forms.Panel pnlDisplay;
         private System.Windows.Forms.Button btnAbout;
         private System.Windows.Forms.Button btnGrades;
